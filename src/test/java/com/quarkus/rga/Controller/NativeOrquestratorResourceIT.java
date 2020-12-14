16d318693e79aef85a2acc6befc14291b9fe2b21
vicente.garcia <vicente.garcia@outlook.com>
2020-12-14 16:07:29 +0100

    new merge delete files and update pom.xml

--- src/test/java/com/quarkus/rga/Controller/NativeOrquestratorResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.quarkus.rga.Controller;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeOrquestratorResourceIT extends OrquestratorResourceTest {
-
-    // Execute the same tests but in native mode.
-}
